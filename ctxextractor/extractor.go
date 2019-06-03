c91b1eec594da1c9ba882def05a94abf99efe981
Alexander Joseph Trelore <alextrelore@gmail.com>
2019-06-03 16:10:02 +0100

    adds a ctx extractor interceptor (#15)
    * adds a ctx extractor interceptor

---
+++ ctxextractor/extractor.go
@@ -0,0 +1,28 @@
+package grpcctxextractor
+
+import (
+	"context"
+
+	grpc_ctxtags "github.com/grpc-ecosystem/go-grpc-middleware/tags"
+	"google.golang.org/grpc"
+)
+
+// Extractor is a function to extract a string out of context
+type Extractor func(context.Context) (string, error)
+
+// UnaryServerInterceptor implements the UnaryServerInterceptor interface
+// Interface is a map with string key which is the key for the log. Plus a function value
+// which extracts the value from context and potentially returning an error.
+func UnaryServerInterceptor(in map[string]Extractor) grpc.UnaryServerInterceptor {
+	return func(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (interface{}, error) {
+		tags := grpc_ctxtags.Extract(ctx)
+		for k, f := range in {
+			s, err := f(ctx)
+			if err != nil {
+				return nil, err
+			}
+			tags = tags.Set(k, s)
+		}
+		return handler(ctx, req)
+	}
+}
